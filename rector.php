b219dca5503ef3234cc102cdbe5036fe6166252e
stereomon <rene.klatt@spryker.com>
2023-10-05 15:50:50 +0200

    DPBC-3017 Updated rector

--- rector.php
+++ rector.php
@@ -1,7 +1,17 @@
 <?php
 
 use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
+
+    $rectorConfig->sets([
+        SetList::PHP_80
+    ]);
+
+    $rectorConfig->ruleWithConfiguration(ClassPropertyAssignToConstructorPromotionRector::class, [
+        ClassPropertyAssignToConstructorPromotionRector::INLINE_PUBLIC => true,
+    ]);
 };
 
