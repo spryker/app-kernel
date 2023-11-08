5f93740b896a7e85de2893b5380a26326a72a086
stereomon <rene.klatt@spryker.com>
2023-11-08 14:52:30 +0100

    stream to string before json_decode

--- src/Spryker/Zed/AppKernel/Business/Reader/ConfigReader.php
+++ src/Spryker/Zed/AppKernel/Business/Reader/ConfigReader.php
@@ -8,12 +8,19 @@
 namespace Spryker\Zed\AppKernel\Business\Reader;
 
 use Generated\Shared\Transfer\AppConfigCriteriaTransfer;
+use Generated\Shared\Transfer\AppConfigTransfer;
+use Spryker\Client\SecretsManager\Exception\MissingSecretsManagerProviderPluginException;
+use Spryker\Client\SecretsManager\SecretsManagerDependencyProvider;
+use Spryker\Client\SecretsManagerExtension\Dependency\Plugin\SecretsManagerProviderPluginInterface;
 use Spryker\Shared\Kernel\Transfer\TransferInterface;
+use Spryker\Shared\Log\LoggerTrait;
 use Spryker\Zed\AppKernel\Business\EncryptionConfigurator\PropelEncryptionConfiguratorInterface;
 use Spryker\Zed\AppKernel\Persistence\AppKernelRepositoryInterface;
 
 class ConfigReader implements ConfigReaderInterface
 {
+    use LoggerTrait;
+
     /**
      * @param \Spryker\Zed\AppKernel\Persistence\AppKernelRepositoryInterface $appKernelRepository
      * @param \Spryker\Zed\AppKernel\Business\EncryptionConfigurator\PropelEncryptionConfiguratorInterface $propelEncryptionConfigurator
@@ -32,8 +39,22 @@ class ConfigReader implements ConfigReaderInterface
      */
     public function getAppConfigByCriteria(AppConfigCriteriaTransfer $appConfigCriteriaTransfer, TransferInterface $transfer): TransferInterface
     {
-        $this->propelEncryptionConfigurator->configurePropelEncryption($appConfigCriteriaTransfer->getTenantIdentifierOrFail());
+        $this->configurePropelEncryption($appConfigCriteriaTransfer);
 
         return $this->appKernelRepository->findAppConfigByCriteria($appConfigCriteriaTransfer, $transfer);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\AppConfigCriteriaTransfer $appConfigCriteriaTransfer
+     *
+     * @return void
+     */
+    protected function configurePropelEncryption(AppConfigCriteriaTransfer $appConfigCriteriaTransfer): void
+    {
+        try {
+            $this->propelEncryptionConfigurator->configurePropelEncryption($appConfigCriteriaTransfer->getTenantIdentifierOrFail());
+        } catch (MissingSecretsManagerProviderPluginException) {
+            $this->getLogger()->warning(sprintf('There is no %s attached to %s::getSecretsManagerProviderPlugin(). This leads to unencrypted data in the database which should be avoided.', SecretsManagerProviderPluginInterface::class, SecretsManagerDependencyProvider::class));
+        }
     }
 }
